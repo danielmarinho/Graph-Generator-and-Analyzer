1961dbf8884e78e2e544f23ee808ca0ef8a16d54
daniel_marinho <daniel_marinho@id.uff.br>
2016-01-31 13:16:37 -0200

    -Programa agora localiza pontes no grafo

--- GraphGeneratorAndAnalyzer/src/graphgeneratorandanalyzer/AnalisadorDeGrafo.java
+++ GraphGeneratorAndAnalyzer/src/graphgeneratorandanalyzer/AnalisadorDeGrafo.java
@@ -21,16 +21,17 @@ public class AnalisadorDeGrafo {
     int[] back;
     int[] cor;
     int t;
-    
+
     ArrayList<ArrayList> componentesConexas = new ArrayList<>();
     ArrayList<Integer> conjuntoConexo = new ArrayList<>();
-    
+
     boolean isBipartido = true;
-    
+
     ArrayList<Integer> articulacoes = new ArrayList<>();
     ArrayList<ArrayList> blocos = new ArrayList<>();
     ArrayList<Aresta> componentesBloco = new ArrayList<>();
-    
+    ArrayList<Aresta> pontes = new ArrayList<>();
+
     ArrayList<Aresta> arestasProfundidade;
     ArrayList<Aresta> arestasRetorno;
 
@@ -54,12 +55,15 @@ public class AnalisadorDeGrafo {
         Integer[] vizinhos;
 
         for (int lin = 0; lin < matrizDeAdjacencia.length; lin++) {
-            if (matrizDeAdjacencia[lin][v] == 1) 
+            if (matrizDeAdjacencia[lin][v] == 1) {
                 vizinhos_aux.add(lin);
-            if(lin==v){
-                for (int col = v+1; col < matrizDeAdjacencia.length; col++) 
-                    if(matrizDeAdjacencia[v][col] == 1)
+            }
+            if (lin == v) {
+                for (int col = v + 1; col < matrizDeAdjacencia.length; col++) {
+                    if (matrizDeAdjacencia[v][col] == 1) {
                         vizinhos_aux.add(col);
+                    }
+                }
             }
         }
         vizinhos = new Integer[vizinhos_aux.size()];
@@ -77,28 +81,35 @@ public class AnalisadorDeGrafo {
 //                marcar vw como aresta "azul" de profundidade
                 Aresta aresta = new Aresta(v, w, Aresta.TIPO.PROFUNDIDADE);
                 arestasProfundidade.add(aresta);
-                
+
                 conjuntoConexo.add(w);
                 componentesBloco.add(aresta);
-                        
+
                 pai[w] = v;
                 cor[w] = 1 - cor[v];
-                
+
                 buscaEmProfundidade(w);
                 back[v] = Integer.min(back[v], back[w]);
-                
-                if(back[w] >= pe[v]){
-                    if(pai[v] == -1){
-                        if(filhos>=2)
-                            if(!articulacoes.contains(v))  articulacoes.add(v);     //v é uma articulação
-                    }else{
-                        if(!articulacoes.contains(v))  articulacoes.add(v);     //v é uma articulação
+                if (back[v] == back[w] - 1) {
+                    pontes.add(aresta);
+                }
+                if (back[w] >= pe[v]) {
+                    if (pai[v] == -1) {
+                        if (filhos >= 2) {
+                            if (!articulacoes.contains(v)) {
+                                articulacoes.add(v);     //v é uma articulação
+                            }
+                        }
+                    } else {
+                        if (!articulacoes.contains(v)) {
+                            articulacoes.add(v);     //v é uma articulação
+                        }
                     }
-                    
+
                     ArrayList<Aresta> blocoAux = new ArrayList<>();
-                    int index = componentesBloco.size()-1;
+                    int index = componentesBloco.size() - 1;
                     Aresta arestaAux = componentesBloco.get(index);
-                    while(!arestaAux.equals(aresta)){
+                    while (!arestaAux.equals(aresta)) {
                         blocoAux.add(arestaAux);
                         componentesBloco.remove(index);
                         index--;
@@ -108,15 +119,15 @@ public class AnalisadorDeGrafo {
                     componentesBloco.remove(index);
                     blocos.add(blocoAux);
                 }
-            } else if ( ps[w] == 0 && w != pai[v] ) {
+            } else if (ps[w] == 0 && w != pai[v]) {
 //                marcar vw como aresta "vermelha" de retorno
                 Aresta aresta = new Aresta(v, w, Aresta.TIPO.RETORNO);
                 arestasRetorno.add(aresta);
                 componentesBloco.add(aresta);
-                
+
                 back[v] = Integer.min(back[v], pe[w]);
-                
-                if(isBipartido && cor[w] == cor[v]){
+
+                if (isBipartido && cor[w] == cor[v]) {
                     isBipartido = false;
                 }
             }
@@ -125,31 +136,47 @@ public class AnalisadorDeGrafo {
         ps[v] = t;
     }
 
-    private boolean isConexo(){
+    private void localizaPontes() {
+        System.out.println("debugger1");
+        for (Aresta aresta : arestasProfundidade) {
+            if (articulacoes.contains(aresta.getOrigem()) && articulacoes.contains(aresta.getDestino())) {
+                pontes.add(aresta);
+            }
+        }
+        for (Aresta aresta : arestasRetorno) {
+            if (articulacoes.contains(aresta.getOrigem()) && articulacoes.contains(aresta.getDestino())) {
+                pontes.add(aresta);
+            }
+        }
+    }
+
+    private boolean isConexo() {
         return componentesConexas.size() == 1;
     }
-    
-    private boolean possuiCiclos(){
+
+    private boolean possuiCiclos() {
         return !arestasRetorno.isEmpty();
     }
-    
-    private boolean isArvore(){
-        if(arestasRetorno.isEmpty() && isConexo()){
+
+    private boolean isArvore() {
+        if (arestasRetorno.isEmpty() && isConexo()) {
             return true;
-        }else{
+        } else {
             return false;
         }
     }
-    
-    public void analisar(){
+
+    public void analisar() {
         conjuntoConexo.add(0);
         buscaEmProfundidade(0);
+        localizaPontes();
+
         //TODO: rever nome variavel aux
         ArrayList<Integer> aux = new ArrayList<>(conjuntoConexo);
         componentesConexas.add(aux);
         conjuntoConexo.clear();
         for (int v = 1; v < matrizDeAdjacencia.length; v++) {
-            if(pe[v] == 0){
+            if (pe[v] == 0) {
                 conjuntoConexo.add(v);
                 buscaEmProfundidade(v);
                 aux = new ArrayList<>(conjuntoConexo);
@@ -162,10 +189,11 @@ public class AnalisadorDeGrafo {
         System.out.println("Possui ciclos? R:" + possuiCiclos());
         System.out.println("É bipartido? R:" + isBipartido);
         System.out.println("Articulações: " + articulacoes.toString());
+        System.out.println("Pontes: " + pontes.toString());
         System.out.println("Componentes Conexas: " + componentesConexas.toString());
         System.out.println("Blocos: " + blocos.toString());
     }
-    
+
     public static void main(String[] args) {
         int[][] matriz = GeradorDeGrafo.executar();
 //        int matriz[][] = {{0,0,1,1,0},{0,0,1,1,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
